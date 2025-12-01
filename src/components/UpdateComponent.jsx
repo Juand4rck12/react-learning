57852c3b72a40702b48e47211d6629fa1d195517
Juan_Diego <juan3312orrego@gmail.com>
2025-12-01 10:49:30 -0500

    Componente de actualización finalizado
    Primera versión

--- src/components/UpdateComponent.jsx
+++ src/components/UpdateComponent.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom"
 
 function UpdateComponent() {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  });
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/users/${id}`)
+      .then(res => {
+        setValues(res.data)
+      })
+      .catch(error => console.error(error));
+  }, [id]);
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    axios.put(`http://localhost:3000/users/${id}`, values)
+      .then(res => {
+        console.log(res);
+        navigate('/');
+      })
+      .catch(error => console.error(error));
+  }
+
   return (
-    <div>
-      update
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center bg-light'>
+      <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
+        <h1>Update a user</h1>
+        <form onSubmit={handleUpdate}>
+          <div className="row mb-2">
+            <div className="col">
+              <label htmlFor="name">Name:</label>
+              <input type="text" name="name" id="name" className='form-control' placeholder='Enter name' required
+                value={values.name} onChange={e => setValues({ ...values, name: e.target.value })} />
+            </div>
+          </div>
+          <div className="row mb-2">
+            <div className="col">
+              <label htmlFor="email">Email:</label>
+              <input type="email" name="email" id="email" className='form-control' placeholder='Enter email' required
+                value={values.email} onChange={e => setValues({ ...values, email: e.target.value })} />
+            </div>
+          </div>
+          <div className="row mb-2">
+            <div className="col">
+              <label htmlFor="phone">Phone:</label>
+              <input type="text" name="phone" id="phone" className='form-control' placeholder='Enter phone' required
+                value={values.phone} onChange={e => setValues({ ...values, phone: e.target.value })} />
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-lg-6">
+              <button className="btn btn-success w-100">
+                <i class="bi bi-floppy"></i> Update
+              </button>
+            </div>
+            <div className="col-lg-6">
+              <Link to={'/'} className="btn btn-secondary w-100">
+                <i class="bi bi-arrow-return-left"></i> Back
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
